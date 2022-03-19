49505481043f0b22e1865caba0b0e880adde9bfe
Mateusz Medon <mateuszmedon1@gmail.com>
2022-03-19 16:44:45 +0100

    Initial commit

---
+++ src/main/java/com/mateuszmedon/guessnumber/demo/number/game/DemoNumberGameApplication.java
@@ -0,0 +1,13 @@
+package com.mateuszmedon.guessnumber.demo.number.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNumberGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNumberGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateuszmedon/guessnumber/demo/number/game/DemoNumberGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszmedon.guessnumber.demo.number.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNumberGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
